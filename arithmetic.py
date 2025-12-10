6ae826ccbcfb8f5674749a9ff6aad8a0b446a65c
Samruddhi Patil <srp392004@gmail.com>
2025-12-10 21:20:11 +0530

    add subtract

---
+++ arithmetic.py
@@ -0,0 +1,6 @@
+def add(a,b):
+    print("a+b= ", a+b)
+
+def substract(a,b):
+    print("a-b= ", a-b)
+
--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import arithmetic
+
+
 print("Hello World!")
+a=int(input("enter a: "))
+b=int(input("enter b: "))
+arithmetic.add(a,b)
+arithmetic.substract(a,b)
