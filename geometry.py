ff5182a0a9ff7d3fb5021b3dd4c99f15e22327fc
Samruddhi Patil <srp392004@gmail.com>
2025-12-10 21:42:35 +0530

    rect area peri

---
+++ geometry.py
@@ -0,0 +1,5 @@
+def calc_rect_area(len,br):
+    print("rect area :",len*br)
+def calc_rect_peri(len,br):
+    print("rec peri:",2*(len+br))
+    
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import arithmetic
-
+import geometry as geo
 
 print("Hello World!")
 a=int(input("enter a: "))
 b=int(input("enter b: "))
 arithmetic.add(a,b)
 arithmetic.substract(a,b)
+
+len=int(input("enter length: "))
+br=int(input("enter br: "))
+
+geo.calc_rect_area(len,br)
+geo.calc_rect_peri(len,br)
