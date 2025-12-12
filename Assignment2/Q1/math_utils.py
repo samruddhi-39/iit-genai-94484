59ad6c575d48867d5a5d8e1614dee3d157f33549
Samruddhi Patil <srp392004@gmail.com>
2025-12-12 19:38:29 +0530

    Deleted Assignment2 folder and its contents

--- Assignment2/Q1/math_utils.py
+++
@@ -1,8 +0,0 @@
-def area_circle(radius):
-    return 3.14 * radius * radius
-
-def area_rectangle(length, width):
-    return length * width
-
-def area_triangle(base, height):
-    return 0.5 * base * height
